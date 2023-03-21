acfa5ea69e3888501722ba5d9b2fbb7967ed2ebd
Rafael Aguiar <rafael.aguiar.im@gmail.com>
2023-03-21 00:23:53 -0300

    Criação do perfil prod

--- src/main/java/br/com/desafio/seasolutions/configs/DevConfig.java
+++ src/main/java/br/com/desafio/seasolutions/configs/DevConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
+@Profile("prod")
 public class DevConfig {
     @Autowired
     private DBServices dbServices;
